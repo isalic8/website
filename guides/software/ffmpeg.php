8a404a3acd915d7efceb1cc09c4ad053dd140029
p <p>
2020-12-13 00:24:44 -0800

    periodic commit

---
+++ guides/software/ffmpeg.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" type="image/x-icon" href="../../files/pix/icon.gif"/>
+	<link rel="stylesheet" href="../../style.css"/>
+	<title>Unixfandom</title>
+</head>
+<body>
+	<div class="main">
+		<div class="header">
+			<p>
+			Unixfandom.com
+			<img src="../../files/pix/penguin.gif" alt="penguin_gif">
+			</p>
+		</div>
+
+		<nav>
+			<?php include 'navigation.php';?>
+		</nav>
+		<h1>Screen recording with ffmpeg</h1>
+		<div class="footer">
+				<?php include 'footer.php';?>
+		</div>
+	</div>
+</body>
+</html>
